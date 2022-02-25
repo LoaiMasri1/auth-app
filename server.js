149fdb6fc87c8c693c5bb14489dfa5b1ab7769a9
lefdawyy <92638982+lefdawyy@users.noreply.github.com>
2022-02-25 17:01:32 +0200

    forget password js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views','index.html'));
-})
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 const db = mysql.createConnection({
     host     :  process.env.HOST,
     user     :  process.env.USER,
-    database : process.env.DATABASE,
+    database :  process.env.DATABASE,
 });
 
 db.connect((err) => {
@@ -38,26 +38,31 @@ app.get('/getusers' , (req , res) => {
     });
 });
 
-app.get('/getusers/:id' , (req , res) => {
-    let sql =`SELECT Username FROM Users WHERE Id = ${req.params.id}` ; 
-    let query = db.query(sql,(err,results) => {
+app.get('/forget',(req,res)=>{
+    res.sendFile(path.join(__dirname,'views','./forgetPassword/forget.html'));
+})
+
+app.get('/forget/user',(req,res)=>{
+
+    let sql=`select * from users where email=${req.query.email} `;
+    let query=db.query(sql,(err,results)=>{
         if(err){
             throw err;
         }
         console.log(results);
-        res.send('users fitched <3')
-    });
-});
-app.post('/add', (req, res, next) => {
-    let post = {id:uuidv4(), username: req.body.name, email: req.body.email, password: req.body.password, age: req.body.age };
-    let sql = 'INSERT INTO users SET ?';
-    let query = db.query(sql, post, (err, results) => {
-        if (err) {
-            throw err;
+        if(results.length>0){ // user found
+            res.redirect(`/newpassword/?email=${req.query.email}`);
+        }else{
+            res.send('user not found'); // user not found
         }
-       res.redirect('/');
+
+        });
     });
-});
+
+app.get('/newpassword',(req,res)=>{
+    res.sendFile(path.join(__dirname,'views','./forgetPassword/NewPassword.html'));
+})
+
 
 app.listen(process.env.PORT || 5000 , () => {
     console.log('Server started on port 3000');
