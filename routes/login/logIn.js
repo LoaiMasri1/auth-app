74a086c9b3e027dad9f57e426e6d407241f9467a
hmoodbas <hmoudahamood@gmail.com>
2022-02-26 23:16:26 +0200

    update username

--- routes/login/logIn.js
+++ routes/login/logIn.js
@@ -3,22 +3,18 @@ const { NULL } = require('mysql/lib/protocol/constants/types');
 const db = require('../../db');
 const router = express.Router();
 
-function getUsername(email){
-    let sql="SELECT username FROM users WHERE email=?";
-    db.query(sql,email, (err, result)=>{
-        if(err) throw err;
-        console.log(result[0].username);
-        return result[0].username;
-    });
-}
-
-router.post('/', (req , res,next) => {
+router.post('/', (req , res, next) => {
     let email = req.body.email
     let password = req.body.password;
     let query = db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (error, results) => {
         if (error) throw error;
         if (results.length > 0) {
-            res.render('mainpage',{email:email});
+            let sql = "SELECT username FROM users WHERE email=?";
+            db.query(sql, email, (err, result) => {
+                if (err) throw err;
+                console.log(result[0].username);
+                res.render('mainpage', { email:result[0].username})
+            });
         } else { res.send('Incorrect Email or Password!'); }
     })
 })
