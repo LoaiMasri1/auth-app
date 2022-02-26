8d4e76b0158dc966e0487488a8c26140f7c32a55
LoaiMasri <s12010098@stu.najah.edu>
2022-02-26 22:50:06 +0200

    get username fun
    not work

--- routes/login/logIn.js
+++ routes/login/logIn.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const { NULL } = require('mysql/lib/protocol/constants/types');
 const db = require('../../db');
 const router = express.Router();
+
+function getUsername(email){
+    let sql="SELECT username FROM users WHERE email=?";
+    db.query(sql,email, (err, result)=>{
+        if(err) throw err;
+        console.log(result[0].username);
+        return result[0].username;
+    });
+}
 
 router.post('/', (req , res,next) => {
     let email = req.body.email
@@ -8,7 +18,7 @@ router.post('/', (req , res,next) => {
     let query = db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (error, results) => {
         if (error) throw error;
         if (results.length > 0) {
-            res.render('mainpage',{email:email})
+            res.render('mainpage',{email:email});
         } else { res.send('Incorrect Email or Password!'); }
     })
 })
