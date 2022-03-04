3fcf0d9fe9904e8de80b569a0e1d4e996c5d7a05
LoaiMasri <s12010098@stu.najah.edu>
2022-03-04 19:26:09 +0200

    error

--- routes/forget/sendCode.js
+++ routes/forget/sendCode.js
@@ -3,6 +3,7 @@ const app = express()
 const router=express.Router();
 const { get } = require('http');
 const nodemailer = require('nodemailer');
+
 if (typeof localStorage === "undefined" || localStorage === null) {
   var LocalStorage = require('node-localstorage').LocalStorage;
   localStorage = new LocalStorage('./scratch');
