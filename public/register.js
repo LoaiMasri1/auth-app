c1a7808caa7bb2afdd69a7927464604656be86c9
lefdawyy <92638982+lefdawyy@users.noreply.github.com>
2022-03-08 18:52:50 +0200

    register data validation update

--- public/register.js
+++ public/register.js
@@ -1,79 +1,101 @@
-let Age = document.getElementById("Age");
-        let Password = document.getElementById("Password");
-        let ConfirmPassword = document.getElementById("ConfirmPassword");
-        let ConfirmAge = document.getElementById("ConfirmAge");
-        let passwordlHelp = document.getElementById("passwordlHelp");
-        let ConfirmpasswordlHelp = document.getElementById("ConfirmpasswordlHelp");
-        let SubmitBtn = document.getElementById("SubmitBtn");
-        let typingTimer;
-        let doneTypingInterval = 1300;  
-        SubmitBtn.addEventListener('click', Main);
-        Password.addEventListener('keyup', PasswordCheck);
-        ConfirmPassword.addEventListener('keyup', test) 
-        function AgeCheck() {
-            if (Age.value >= 18) {
-                ConfirmAge.innerHTML = "Age must be over 18 years old";
-                ConfirmAge.style.color = "green";
-                SubmitBtn.disabled = false;
-            } else {
-                ConfirmAge.innerHTML = "You are less than 18 years old, the operation cannot be completed";
-                ConfirmAge.style.color = "red";
-                SubmitBtn.disabled = true;
+const email= document.getElementById('Email');
+const password= document.getElementById('Password');
+const confirmPassword= document.getElementById('ConfirmPassword');
 
-            }
-        }
-        function PasswordCheck() {
-            if (Password.value.length < 8) {
-                passwordlHelp.innerHTML = "Password is less than 8 characters";
-                passwordlHelp.style.color = "red";
-                SubmitBtn.disabled = true;
-                return false
-            }
-            if (Password.value.length > 20) {
-                passwordlHelp.innerHTML = "Password more than 20 characters";
-                passwordlHelp.style.color = "red";
-                SubmitBtn.disabled = true;
-                return false
-            }
-            if (checkPassword(Password.value)) {
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+const AGE_REGEX = /^(?:1[01][0-9]|120|1[7-9]|[2-9][0-9])$/;
+const TIME_DELAY = 500;
 
-                passwordlHelp.innerHTML = "The password is valid";
-                passwordlHelp.style.color = "green";
-                SubmitBtn.disabled = false;
+let emailTimeout;
+let passwordTimeout;
+let ageTimeout;
 
-            } else {
-                SubmitBtn.disabled = true;
-                passwordlHelp.innerHTML = "The password is not valid";
-                passwordlHelp.style.color = "red";
-            }
-        }
+function validateEmail() {
+    if (EMAIL_REGEX.test(email.value)) {
+        clearTimeout(emailTimeout);
+        emailTimeout = setTimeout(() => {
+            email.classList.remove("is-invalid");
+            email.classList.add("is-valid");
+            SubmitBtn.disabled = false;
+        }, TIME_DELAY);
+    } else {
+        clearTimeout(emailTimeout);
+        email.classList.remove("is-valid");
+        email.classList.add("is-invalid");
+        SubmitBtn.disabled = true;
+    }
+}
 
-        function checkPassword(str) {
-            var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
-            return re.test(str);
-        }
-        function ConfirmPasswordCheck() {
-            if (Password.value == ConfirmPassword.value && Password.value != "" && ConfirmPassword.value != "") {
-                ConfirmpasswordlHelp.innerHTML = "The two password match";
-                ConfirmpasswordlHelp.style.color = "green";
-                SubmitBtn.disabled = false;
-            } else {
-                ConfirmpasswordlHelp.innerHTML = "The two password don't match";
-                ConfirmpasswordlHelp.style.color = "red";
-                SubmitBtn.disabled = true;
-            }
-        }
-        function test(){
-            clearTimeout(typingTimer);
-            if (ConfirmPassword.value) {
-                typingTimer = setTimeout(doneTyping, doneTypingInterval);
-            }
-        }
-        function doneTyping() {
-            ConfirmPasswordCheck();
-        }
-        function Main() {
-            AgeCheck();
-            PasswordCheck();
-            ConfirmPasswordCheck();
-        }
+
+
+function validatePassword() {
+    if (PASSWORD_REGEX.test(password.value) && password.value.length >= 8) {
+        clearTimeout(passwordTimeout);
+        passwordTimeout = setTimeout(() => {
+            password.classList.remove("is-invalid");
+            password.classList.add("is-valid");
+            SubmitBtn.disabled = false;
+        }, TIME_DELAY);
+    } else {
+        clearTimeout(passwordTimeout);
+        password.classList.remove("is-valid");
+        password.classList.add("is-invalid");
+        SubmitBtn.disabled = true;
+    }
+}
+
+function validateConfirmPassword() {
+    if (password.value == confirmPassword.value && confirmPassword.value.length >= 8) {
+        clearTimeout(passwordTimeout);
+        confirmPassword.classList.remove("is-invalid");
+        confirmPassword.classList.add("is-valid");
+        SubmitBtn.disabled = false;
+    } else {
+        clearTimeout(passwordTimeout);
+        confirmPassword.classList.remove("is-valid");
+        confirmPassword.classList.add("is-invalid");
+        SubmitBtn.disabled = true;
+    }
+}
+
+function validateAge() {
+    if (AGE_REGEX.test(document.getElementById("Age").value)) {
+        clearTimeout(ageTimeout);
+        ageTimeout = setTimeout(() => {
+            document.getElementById("Age").classList.remove("is-invalid");
+            document.getElementById("Age").classList.add("is-valid");
+            SubmitBtn.disabled = false;
+        }, TIME_DELAY);
+    } else {
+        clearTimeout(ageTimeout);
+        document.getElementById("Age").classList.remove("is-valid");
+        document.getElementById("Age").classList.add("is-invalid");
+        SubmitBtn.disabled = true;
+    }
+}
+
+function validateAll() {
+    if (EMAIL_REGEX.test(email.value) &&PASSWORD_REGEX.test(password.value) && AGE_REGEX.test(document.getElementById("Age").value) && password.value.length >= 8 && document.getElementById("Age").value.length >= 1) {
+        SubmitBtn.disabled = false;
+    } else {
+        SubmitBtn.disabled = true;
+    }
+}
+
+// on input change
+password.addEventListener("input", validatePassword);
+confirmPassword.addEventListener("input", validateConfirmPassword);
+document.getElementById("Age").addEventListener("input", validateAge);
+email.addEventListener("input", validateEmail);
+
+
+// on blur
+password.addEventListener("blur", validatePassword);
+confirmPassword.addEventListener("blur", validateConfirmPassword);
+document.getElementById("Age").addEventListener("blur", validateAge);
+email.addEventListener("blur", validateEmail);
+// on submit
+document.getElementById("SubmitBtn").addEventListener("click", () => {
+    validateAll();
+});
